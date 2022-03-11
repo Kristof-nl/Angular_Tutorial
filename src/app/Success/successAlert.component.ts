1db07bda05a7137775155a744689aa79a97956a7
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-03-11 15:32:10 +0100

    Binding

--- src/app/Success/successAlert.component.ts
+++ src/app/Success/successAlert.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-success-alert',
-    templateUrl: './successAlert.component.html'
+    templateUrl: './successAlert.component.html',
+    styleUrls: ['./successAlert.component.css']
 
 })
 export class SuccessAlertComponent {
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>, 
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreactionStatus: string = "No server was found";
+  serverName = "";
 
-  constructor() { }
+  constructor()  {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreactionStatus = "Server was created";
+  }
+  onUpdateServerName(event :Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+
   }
 
 }
